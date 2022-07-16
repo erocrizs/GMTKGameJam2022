6d83689c0f29fd237600cfa8cb94fb7b2c08a637
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2022-07-16 17:23:04 +0800

    Refactor rolling calculation

--- Assets/Scripts/Die/DieMovement.cs
+++ Assets/Scripts/Die/DieMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,11 @@ public class DieMovement : MonoBehaviour
     float rollTime;
 
     bool isMoving = false;
-    bool isGrounded = false;
-
     float moveLerp = 1;
     float moveDirection = 0;
     Vector2 preMovementPosition;
     Rigidbody2D rb;
+    Action<float> Roll;
 
     private Vector2 RoundVector (Vector2 v)
     {
@@ -22,33 +22,32 @@ public class DieMovement : MonoBehaviour
         return new Vector2(x, y);
     }
 
-    private Vector2 LerpPosition (float degree, float direction)
+    private Vector2 LerpPosition (float degree, float rotationalDirection, Vector2 corner)
     {
-        float directionSign = Mathf.Sign(direction);
-        Vector2 cornerToCenter = Quaternion.Euler(0, 0, directionSign * 45) * Vector2.up * (Mathf.Sqrt(2) / 2);
-        Vector2 rotatedCornerToCenter = Quaternion.Euler(0, 0, -1f * degree * directionSign) * cornerToCenter;
-        Vector2 corner = preMovementPosition + new Vector2(directionSign * 0.5f, -0.5f);
-        Debug.Log(preMovementPosition + " " + rotatedCornerToCenter + " " + corner + " " + (corner + rotatedCornerToCenter));
-        return corner + rotatedCornerToCenter;
-    }
+        Vector2 cornerPosition = preMovementPosition + corner;
+        Vector2 cornerToCenter = -corner;
+        Vector2 rotatedCornerToCenter = Quaternion.Euler(0, 0, degree * rotationalDirection) * cornerToCenter;
 
+        return cornerPosition + rotatedCornerToCenter;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Roll = RollHorizontal;
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << LayerMask.NameToLayer("Ground"));
-        float movement = Input.GetAxis("Horizontal");
-        if (!isMoving && isGrounded)
+        if (!isMoving)
         {
+            bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << LayerMask.NameToLayer("Ground"));
+            float movement = Input.GetAxis("Horizontal");
             isMoving = movement >= 0.01 || movement <= -0.01;
-            if (isMoving)
+            if (isMoving && isGrounded)
             {
-                Debug.Log("Moving");
                 moveLerp = 0;
                 moveDirection = movement;
                 rb.isKinematic = true;
@@ -64,22 +63,46 @@ public class DieMovement : MonoBehaviour
         {
             float progress = Mathf.Min(Time.fixedDeltaTime / rollTime, 1 - moveLerp);
             moveLerp += progress;
-            float degree = moveLerp * 90;
-            transform.position = LerpPosition(degree, moveDirection);
-            transform.Rotate(0, 0, progress * -90 * Mathf.Sign(moveDirection));
-
-            if (moveLerp == 1)
-            {
-                Debug.Log("Stop");
-                isMoving = false;
-                rb.isKinematic = false;
-                rb.simulated = true;
-                rb.position = RoundVector(rb.position);
-            }
+            Roll(progress);
         }
         else
         {
             rb.position = new Vector2(Mathf.RoundToInt(rb.position.x), rb.position.y);
         }
+    }
+
+    void RollHorizontal (float progress)
+    {
+        float degree = moveLerp * 90;
+        float rotationalDirection = -1 * Mathf.Sign(moveDirection);
+        Vector2 corner = new Vector2(Mathf.Sign(moveDirection) * 0.5f, -0.5f);
+
+        transform.position = LerpPosition(degree, rotationalDirection, corner);
+        transform.Rotate(0, 0, progress * -90 * Mathf.Sign(moveDirection));
+
+        if (moveLerp == 1)
+        {
+            // bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, 1 << LayerMask.NameToLayer("Ground"));
+            
+            RollStop();
+        }
+    }
+
+    void RollUp (float progress)
+    {
+        
+    }
+
+    void RollDown (float progress)
+    {
+        // TODO
+    }
+
+    void RollStop ()
+    {
+        isMoving = false;
+        rb.isKinematic = false;
+        rb.simulated = true;
+        rb.position = RoundVector(rb.position);
     }
 }
