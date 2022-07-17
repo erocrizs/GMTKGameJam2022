4ad2dcfc26da3fab6c74094390a67787eb53c461
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2022-07-18 02:02:53 +0800

    Last minute build updates

--- Assets/Scripts/DieDetector.cs
+++ Assets/Scripts/DieDetector.cs
@@ -22,7 +22,7 @@ public class DieDetector : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Die"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Die") || collision.gameObject.layer == LayerMask.NameToLayer("DieMoving"))
         {
             isDieInside = true;
 
@@ -42,7 +42,7 @@ public class DieDetector : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Die"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Die") || collision.gameObject.layer == LayerMask.NameToLayer("DieMoving"))
         {
             isDieInside = false;
 
