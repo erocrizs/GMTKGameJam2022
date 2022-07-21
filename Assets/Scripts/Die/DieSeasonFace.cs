c7c4c718b3d06baef39a8b41b4dd4829676429a0
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2022-07-22 01:27:58 +0800

    Make DieSeasonFace extensible

--- Assets/Scripts/Die/DieSeasonFace.cs
+++ Assets/Scripts/Die/DieSeasonFace.cs
@@ -5,6 +5,15 @@ public class DieSeasonFace : MonoBehaviour
 {
     public Season dieSeason = Season.Spring;
 
+    [SerializeField]
+    Season topSeason;
+    [SerializeField]
+    Season rightSeason;
+    [SerializeField]
+    Season bottomSeason;
+    [SerializeField]
+    Season leftSeason;
+
     public void Update()
     {
         float zRotation = gameObject.transform.rotation.eulerAngles.z;
@@ -12,19 +21,19 @@ public class DieSeasonFace : MonoBehaviour
 
         if (Mathf.Abs(zRotation - 0) < tolerance)
         {
-            dieSeason = Season.Spring;
+            dieSeason = topSeason;
         }
         else if (Mathf.Abs(zRotation - 90) < tolerance)
         {
-            dieSeason = Season.Summer;
+            dieSeason = rightSeason;
         }
         else if (Mathf.Abs(zRotation - 180) < tolerance)
         {
-            dieSeason = Season.Autumn;
+            dieSeason = bottomSeason;
         }
         else if (Mathf.Abs(zRotation - 270) < tolerance)
         {
-            dieSeason = Season.Winter;
+            dieSeason = leftSeason;
         }
     }
 }
