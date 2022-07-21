f7d3d2e51f1f04fe3cf7d1a972a24e60631010bc
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2022-07-22 00:55:52 +0800

    Reimplement seasonal activation

--- Assets/Scripts/Season/SeasonalActivation.cs
+++ Assets/Scripts/Season/SeasonalActivation.cs
@@ -1,63 +1,35 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class SeasonalActivation : MonoBehaviour
 {
     [SerializeField]
-    GameObject springObject;
-    [SerializeField]
-    GameObject summerObject;
-    [SerializeField]
-    GameObject autumnObject;
-    [SerializeField]
-    GameObject winterObject;
-
-    Dictionary<Season, GameObject> seasonObjectMapping;
-
-    // Start is called before the first frame update
+    GameObject defaultActive;
+    SeasonalActivationChild[] children;
     void Start()
     {
-        seasonObjectMapping = new Dictionary<Season, GameObject>
-        {
-            { Season.Spring, springObject },
-            { Season.Summer, summerObject },
-            { Season.Autumn, autumnObject },
-            { Season.Winter, winterObject }
-        };
-
-        new SeasonObserver().SubscribeToSeason(ChangeState, true);
+        children = GetComponentsInChildren<SeasonalActivationChild>(true);
+        new SeasonObserver().SubscribeToSeason(ActivateChildren, true);
     }
 
-    private void ChangeState (Season season)
+    void ActivateChildren (Season season)
     {
-        HashSet<GameObject> toActivate = new HashSet<GameObject>();
-        HashSet<GameObject> toDeactivate = new HashSet<GameObject>();
-        
-        foreach (var item in seasonObjectMapping)
-        {
-            if (item.Value == null)
-            {
-                continue;
-            }
+        bool hasActiveChildren = false;
 
-            if (item.Key == season)
+        foreach (var child in children)
+        {
+            child.ChangeState(season);
+
+            if (child.isActiveAndEnabled)
             {
-                toActivate.Add(item.Value);
-            } else
-            {
-                toDeactivate.Add(item.Value);
+                hasActiveChildren = true;
             }
         }
 
-        toDeactivate.ExceptWith(toActivate);
-        foreach (var seasonObject in toActivate)
+        if (defaultActive && hasActiveChildren == defaultActive.activeInHierarchy)
         {
-            seasonObject.SetActive(true);
-        }
-        foreach (var seasonObject in toDeactivate)
-        {
-            seasonObject.SetActive(false);
+            defaultActive.SetActive(!hasActiveChildren);
         }
     }
 }
