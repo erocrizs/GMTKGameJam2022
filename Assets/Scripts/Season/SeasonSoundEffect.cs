60d3cb48e6d2bafde8efbfe5e99e97285c13417d
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2022-07-22 01:21:43 +0800

    Make SeasonSoundEffect extensible

--- Assets/Scripts/Season/SeasonSoundEffect.cs
+++ Assets/Scripts/Season/SeasonSoundEffect.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,29 +6,34 @@ using UnityEngine;
 public class SeasonSoundEffect : MonoBehaviour
 {
     [SerializeField]
-    AudioClip springAudio;
-    [SerializeField]
-    AudioClip summerAudio;
-    [SerializeField]
-    AudioClip autumnAudio;
-    [SerializeField]
-    AudioClip winterAudio;
+    SeasonalAudioClip[] audioClips;
+
     Dictionary<Season, AudioClip> seasonAudioMapping;
     AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        seasonAudioMapping = new Dictionary<Season, AudioClip>
+        seasonAudioMapping = new Dictionary<Season, AudioClip>();
+        foreach (var pair in audioClips)
         {
-            { Season.Spring, springAudio },
-            { Season.Summer, summerAudio },
-            { Season.Autumn, autumnAudio },
-            { Season.Winter, winterAudio }
-        };
+            seasonAudioMapping[pair.season] = pair.audioClip;
+        }
+        audioClips = null;
         audioSource = GetComponent<AudioSource>();
         SeasonManager.SubscribeToSeason(PlaySFX, false);
     }
 
-    private void PlaySFX (Season season) => audioSource.PlayOneShot(seasonAudioMapping[season]);
+    private void PlaySFX(Season season)
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(seasonAudioMapping[season]);
+    }
+
+    [Serializable]
+    private struct SeasonalAudioClip
+    {
+        public Season season;
+        public AudioClip audioClip;
+    }
 }
