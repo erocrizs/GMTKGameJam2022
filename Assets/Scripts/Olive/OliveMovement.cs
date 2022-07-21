9c298006d3403638d9a47a174f5344bcea906eae
EroCrizs <aemielvin.loremia@gmail.com>
2022-07-21 21:53:35 +0800

    Improve horizontal movement physics

--- Assets/Scripts/Olive/OliveMovement.cs
+++ Assets/Scripts/Olive/OliveMovement.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class OliveMovement : MonoBehaviour
 {
+    float movement;
     [SerializeField]
-    float runSpeed;
-
+    float maxRunSpeed;
+    [SerializeField]
+    float runAccelerationTime;
+    float RunAcceleration => maxRunSpeed / runAccelerationTime;
+    [SerializeField]
+    float runDecelerationTime;
+    float RunDeceleration => maxRunSpeed / runDecelerationTime;
+    [SerializeField]
+    float turnAccelerationTime;
+    float TurnAcceleration => maxRunSpeed / turnAccelerationTime;
     [SerializeField]
     float burstJumpSpeed;
-
     [SerializeField]
     float maxFallSpeed;
 
@@ -60,15 +68,29 @@ public class OliveMovement : MonoBehaviour
 
     void Move ()
     {
-        float movement = Input.GetAxis("Horizontal Olive");
-        if (movement >= 0.01 || movement <= -0.01)
+        movement = Input.GetAxisRaw("Horizontal Olive");
+
+        // moving
+        if (movement != 0)
         {
-            rb.velocity = new Vector2(Mathf.Sign(movement) * runSpeed, rb.velocity.y);
+            float acceleration = (Mathf.Sign(movement) == Mathf.Sign(rb.velocity.x)) ? RunAcceleration : TurnAcceleration;
+            float acceleratedVxMagnitude = rb.velocity.x + (Mathf.Sign(movement) * acceleration * Time.deltaTime);
+            float cappedVxMagnitude = Mathf.Clamp(acceleratedVxMagnitude, -maxRunSpeed, maxRunSpeed);
+            rb.velocity = new Vector2(cappedVxMagnitude, rb.velocity.y);
         }
-        else
+        // slowing down
+        else if (Mathf.Abs(rb.velocity.x) > 0)
         {
+            float acceleratedVxMagnitude = rb.velocity.x + (-Mathf.Sign(rb.velocity.x) * RunDeceleration * Time.deltaTime);
+            float cappedVxMagnitude = (Mathf.Sign(rb.velocity.x) == Mathf.Sign(acceleratedVxMagnitude)) ? acceleratedVxMagnitude : 0f;
+            rb.velocity = new Vector2(cappedVxMagnitude, rb.velocity.y);
+        }
+        // maintain stop
+        else {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+
+        velocity = rb.velocity.x;
     }
 
     void Jump ()
@@ -91,7 +113,7 @@ public class OliveMovement : MonoBehaviour
 
     void Animate ()
     {
-        animator.SetDirection(rb.velocity.x);
+        animator.SetDirection(movement == 0 ?  rb.velocity.x : movement);
         animator.IsFalling = IsFalling;
         animator.IsRising = IsRising;
         animator.IsRunning = IsRunning;
